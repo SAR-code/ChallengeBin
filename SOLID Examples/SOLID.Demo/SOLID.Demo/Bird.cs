d2b82f3118eedcd697d6ffe2f70f5df2c76cce67
SAR-code <dylanmccallum28@gmail.com>
2023-08-23 23:34:30 -0700

    SOLID Example

---
+++ SOLID Examples/SOLID.Demo/SOLID.Demo/Bird.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Demo
+{
+    public class Bird
+    {
+        public void Chirp() {
+            Console.WriteLine("Chuuuurp");
+        }
+    }
+
+    public class FlyingBird : Bird 
+    {
+        public void Fly(){
+            Console.WriteLine("This bird is airborne");
+        }
+    }
+
+    public class Ostrich : Bird
+    {
+
+    }
+
+    public class Pigeon : FlyingBird { }
+}
