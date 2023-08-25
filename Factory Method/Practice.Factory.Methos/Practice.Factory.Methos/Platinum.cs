b35887f851dd9056dd3bae04217df1547fe27621
SAR-code <dylanmccallum28@gmail.com>
2023-08-25 00:57:10 -0700

    Factory Pattern W/ ISP

---
+++ Factory Method/Practice.Factory.Methos/Practice.Factory.Methos/Platinum.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice.Factory.Methos
+{
+    public class Platinum : CreditCard
+    {
+        public override string GetCardType()
+        {
+            return "Platinum Plus";
+        }
+
+        public  override int GetCreditLimit()
+        {
+            return 35000;
+        }
+
+        public override int GetAnnualCharge()
+        {
+            return 2000;
+        }
+    }
+}
