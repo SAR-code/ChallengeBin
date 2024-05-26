2649f132895f513a5159cebf5891878f5a375bb9
SAR-code <dylanmccallum28@gmail.com>
2024-05-25 17:30:37 -0700

    check

--- Snippets/snippets/src/components/snippet-edit-form.tsx
+++ Snippets/snippets/src/components/snippet-edit-form.tsx
@@ -5,8 +5,6 @@ interface SnippetEditFormProps {
   snippet: Snippet;
 }
 
-//check
-
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
   return <div>Client component has snippet title {snippet.title}</div>;
 }
